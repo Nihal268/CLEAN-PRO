39de26431462d5003397b91501023b2db1b95183
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-08-16 11:03:14 +0530

    Updates in controiller

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import { sendOtp } from "../controllers/userController";
 
 const userRoute = express.Router();
 
-userRoute.post('/send-otp', sendOtp)
 
 
 export default userRoute ;
