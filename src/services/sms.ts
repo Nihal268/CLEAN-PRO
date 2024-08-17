c0a0cd22c39bf7d7b5a9e6dc8ae4e135e37f91cc
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2024-08-16 21:08:02 -0700

    Updated

--- src/services/sms.ts
+++ src/services/sms.ts
@@ -1,9 +1,46 @@
 
-export const sendSMS = async (mobile: string, otp: string) => {
+const nodemailer=require('nodemailer')
+
+
+const password=process.env.PASSWORD;
+const adminEmail=process.env.EMAIL;
+
+
+export const sendSMS = async (name:string, email:string, otp:number) => {
   try {
-    return true
+
+    const transporter = nodemailer.createTransport({
+      host: 'SMTP.gmail.com', 
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      service:'gmail',
+      
+      auth: {
+        user: 'nihalmuhaednihal@gmail.com',
+        pass: 'wfge xhpl ejaq tnpw'
+        
+      }
+    });
+    const mailOptions = {
+      from: 'nihalmuhaednihal@gmail.com',
+      to: email,
+      subject: 'Email Verification',
+      html: `<p>Hi ${name}, please  check your otp for ${otp} to verify your email.</p>`
+    };
+
+    transporter.sendMail(mailOptions,  (error:any, info:any)=> {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email has been sent:', info.response);
+      }
+    });
+  } catch (error:any) {
+    console.log(error.message);
   }
-  catch (error) {
-    console.error(error);
-  }
-}
+};
+
+
+
+
